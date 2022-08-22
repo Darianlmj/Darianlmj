db1aa6209512157871e9c8250a9ed6e033641236
Darian Lee <darianlmj@gmail.com>
2022-08-22 14:48:38 +1000

    chore(style): cleaned up code style in TechStackDisplay to make it more maintainable

--- src/components/TechStackDisplay/index.tsx
+++ src/components/TechStackDisplay/index.tsx
@@ -3,43 +3,20 @@ import { DisplayContainer, Pill, Image } from './styles';
 import { useStore } from '@nanostores/react';
 import { activePill } from 'src/stores/store';
 
-export const TechStackDisplay = () => {
+export const TechStackDisplay = (): JSX.Element => {
   const $activePill = useStore(activePill);
 
   return (
     <DisplayContainer>
       {
-        $activePill === 'languages' &&
-        skills.map(skill => (
-          skill.languages.map(language => (
-            <Pill key={language.name}>
-              <Image src={language.link} alt={language.name} />
-              {language.name}
+        skills.map((skill: object) =>
+          skill[$activePill].map((item: any, index: number) => (
+            <Pill key={index}>
+              <Image src={item.link} alt={item.name} />
+              {item.name}
             </Pill>
           ))
-        ))
-      }
-      {
-        $activePill === 'frameworks' &&
-        skills.map(skill => (
-          skill.frameworks.map(framework => (
-            <Pill key={framework.name}>
-              <Image src={framework.link} alt={framework.name} />
-              {framework.name}
-            </Pill>
-          ))
-        ))
-      }
-      {
-        $activePill === 'tools' &&
-        skills.map(skill => (
-          skill.tools.map(tool => (
-            <Pill key={tool.name}>
-              <Image src={tool.link} alt={tool.name} />
-              {tool.name}
-            </Pill>
-          ))
-        ))
+        ) 
       }
     </DisplayContainer>
   );
