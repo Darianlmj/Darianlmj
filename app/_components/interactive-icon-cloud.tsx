cb339617f1b610ba37db4eaf3ce8aeff4631ded2
Darian Lee <darian.l@covergenius.com>
2025-02-27 22:29:57 +1100

    bugfix: issue with icon cloud

--- app/_components/interactive-icon-cloud.tsx
+++ app/_components/interactive-icon-cloud.tsx
@@ -1,4 +1,4 @@
-import IconCloud from '@/components/ui/icon-cloud';
+import { IconCloud } from '@/components/ui/icon-cloud'
 
 const slugs = [
   'typescript',
@@ -42,14 +42,20 @@ const slugs = [
   'astro',
   'htmx',
   'express',
-  'sass',
-];
+  'sass'
+]
 
 export function InteractiveIconCloud() {
+  const images = slugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}`
+  )
+
   return (
-    <div className='relative flex size-full min-w-[25rem] max-w-2xl items-center justify-center overflow-hidden rounded-lg px-20'>
-      <IconCloud iconSlugs={slugs} />
-      <h3 className='absolute px-2 py-2 top-[50%] backdrop-blur-sm rounded-md z-10 scroll-m-20 pb-2 text-lg md:text-3xl font-semibold tracking-tight first:mt-0'>Pokèmon</h3>
+    <div className="relative flex size-full min-w-[25rem] max-w-2xl items-center justify-center overflow-hidden rounded-lg px-20">
+      <IconCloud images={images} />
+      <h3 className="absolute px-2 py-2 top-[45%] backdrop-blur-sm rounded-md z-10 scroll-m-20 pb-2 text-lg md:text-3xl font-semibold tracking-tight first:mt-0">
+        Pokèmon
+      </h3>
     </div>
-  );
+  )
 }
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const cameras = {
+  SONY_A7III: 'Sony A7 III',
+  CANON_G7X: 'Canon G7X Mark II',
+  FUJIFILM_X100VI: 'Fujifilm X100VI',
+  KODAK: 'Kodak Powerflash',
+}
