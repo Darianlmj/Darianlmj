9f4028ca650d8367dec0d33e4b0717ae1ad4c940
Darian Lee <darian.l@covergenius.com>
2025-05-29 21:42:58 +1000

    fix: hydration errors in console

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className='scroll-smooth'>
+    <html lang='en' className='scroll-smooth' suppressHydrationWarning>
       <body
         className={`${inter.variable} ${jetBrainsMono.variable} antialiased`}
       >
