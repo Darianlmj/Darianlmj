d323adbd364d8240852facf089ca42881a7e0206
Darian Lee <darian.l@covergenius.com>
2025-06-03 19:04:41 +1000

    chore: update assets

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'group flex flex-1 items-center justify-between py-4 text-xl font-semibold hover:underline hover:underline-offset-2 hover:decoration-[#438EDF] hover:decoration-2 text-left [&[data-state=open]>svg]:rotate-180',
+        'group flex flex-1 items-center justify-between py-4 text-2xl font-semibold hover:underline hover:underline-offset-2 hover:decoration-[#438EDF] hover:decoration-2 text-left [&[data-state=open]>svg]:rotate-180',
         className
       )}
       {...props}
